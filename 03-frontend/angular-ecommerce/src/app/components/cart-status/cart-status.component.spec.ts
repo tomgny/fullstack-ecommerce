7b1b9de2b0605d7cfb90844f29e554970e7434f9
Tomasz Gnyp <tomasz.gnyp4@gmail.com>
2021-03-25 18:30:41 +0100

    First commit

---
+++ 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { CartStatusComponent } from "./cart-status.component";
+
+describe("CartStatusComponent", () => {
+    let component: CartStatusComponent;
+    let fixture: ComponentFixture<CartStatusComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CartStatusComponent],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CartStatusComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+});
