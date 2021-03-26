2539c6c94a43507d1515cfceda31b72b48c86cda
Tomasz Gnyp <tomasz.gnyp4@gmail.com>
2021-03-27 00:59:10 +0100

    Add protected routes and handling browser refresh by local storage

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -22,13 +22,15 @@ import { LoginStatusComponent } from './components/login-status/login-status.com
 import {
     OKTA_CONFIG,
     OktaAuthModule,
-    OktaCallbackComponent
+    OktaCallbackComponent,
+    OktaAuthGuard
 } from '@okta/okta-angular';
 import myAppConfig from './config/my-app-config';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = Object.assign(
     {
-        onAuthRequired: injector => {
+        onAuthRequired: (oktaAuth, injector) => {
             const router = injector.get(Router);
 
             //Redirect the user to your custom login page
@@ -39,6 +41,11 @@ const oktaConfig = Object.assign(
 );
 
 const routes: Routes = [
+    {
+        path: 'members',
+        component: MembersPageComponent,
+        canActivate: [OktaAuthGuard]
+    },
     { path: 'login/callback', component: OktaCallbackComponent },
     { path: 'login', component: LoginComponent },
     { path: 'checkout', component: CheckoutComponent },
@@ -63,7 +70,8 @@ const routes: Routes = [
         CartDetailsComponent,
         CheckoutComponent,
         LoginComponent,
-        LoginStatusComponent
+        LoginStatusComponent,
+        MembersPageComponent
     ],
     imports: [
         RouterModule.forRoot(routes),
--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -11,7 +11,23 @@ export class CartService {
     totalPrice: BehaviorSubject<number> = new BehaviorSubject<number>(0);
     totalQuantity: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-    constructor() {}
+    storage: Storage = localStorage;
+
+    constructor() {
+        //read data from storage
+        let data = JSON.parse(this.storage.getItem('cartItems'));
+
+        if (data != null) {
+            this.cartItems = data;
+
+            //compute totals based on the data that is read from storage
+            this.computeCartTotals();
+        }
+    }
+
+    persistCartItems() {
+        this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
+    }
 
     addToCart(theCartItem: CartItem) {
         // check if we already have the item in our cart
@@ -57,6 +73,9 @@ export class CartService {
 
         // log cart data just for debugging purposes
         this.logCartData(totalPriceValue, totalQuantityValue);
+
+        //persist cart data
+        this.persistCartItems();
     }
 
     logCartData(totalPriceValue: number, totalQuantityValue: number) {
